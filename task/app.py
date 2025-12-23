7da32363c710b02bfb4e174fea6be2070228248d
fenix23707 <fenix23707@gmail.com>
2025-12-23 10:03:14 +0300

    Implemented tasks

--- task/app.py
+++ task/app.py
@@ -1,33 +1,36 @@
 import asyncio
 
 from task.clients.client import DialClient
+from task.clients.custom_client import DialClient as CustomDialClient
 from task.constants import DEFAULT_SYSTEM_PROMPT
 from task.models.conversation import Conversation
 from task.models.message import Message
 from task.models.role import Role
 
 
-async def start(stream: bool) -> None:
+async def start(stream: bool, client) -> None:
+
+    conversation = Conversation()
+    system_prompt = input("Enter system prompt (or press Enter to use default): ").strip() or DEFAULT_SYSTEM_PROMPT
+    conversation.add_message(Message(role=Role.SYSTEM, content=system_prompt))
+
+    while True:
+        user_message = input("Enter your message('exit' to stop): ").strip()
+        if user_message.lower() == 'exit':
+            return
+
+        conversation.add_message(Message(role=Role.USER, content=user_message))
+        if stream:
+            response = await client.stream_completion(conversation.get_messages())
+        else:
+            response = client.get_completion(conversation.get_messages())
+        conversation.add_message(response)
+
     #TODO:
-    # 1.1. Create DialClient
-    # (you can get available deployment_name via https://ai-proxy.lab.epam.com/openai/models
-    #  you can import Postman collection to make a request, file in the project root `dial-basics.postman_collection.json`
-    #  don't forget to add your API_KEY)
-    # 1.2. Create CustomDialClient
-    # 2. Create Conversation object
-    # 3. Get System prompt from console or use default -> constants.DEFAULT_SYSTEM_PROMPT and add to conversation
-    #    messages.
-    # 4. Use infinite cycle (while True) and get yser message from console
-    # 5. If user message is `exit` then stop the loop
-    # 6. Add user message to conversation history (role 'user')
-    # 7. If `stream` param is true -> call DialClient#stream_completion()
-    #    else -> call DialClient#get_completion()
-    # 8. Add generated message to history
-    # 9. Test it with DialClient and CustomDialClient
     # 10. In CustomDialClient add print of whole request and response to see what you send and what you get in response
-    raise NotImplementedError
 
+# asyncio.run(start(False, DialClient(deployment_name="gpt-4")))
+# asyncio.run(start(True, DialClient(deployment_name="gpt-4")))
 
-asyncio.run(
-    start(True)
-)
+# asyncio.run(start(False, CustomDialClient(deployment_name="gpt-4")))
+asyncio.run(start(True, CustomDialClient(deployment_name="gpt-4")))
--- task/clients/custom_client.py
+++ task/clients/custom_client.py
@@ -8,43 +8,48 @@ from task.models.message import Message
 from task.models.role import Role
 
 
-class DialClient:
+class DialClient(BaseClient):
     _endpoint: str
     _api_key: str
 
     def __init__(self, deployment_name: str):
         super().__init__(deployment_name)
+        self.headers = {
+            "api-key": self._api_key,
+            "Content-Type": "application/json"
+        }
         self._endpoint = DIAL_ENDPOINT + f"/openai/deployments/{deployment_name}/chat/completions"
 
     def get_completion(self, messages: list[Message]) -> Message:
-        #TODO:
-        # Take a look at README.md of how the request and regular response are looks like!
-        # 1. Create headers dict with api-key and Content-Type
-        # 2. Create request_data dictionary with:
-        #   - "messages": convert messages list to dict format using msg.to_dict() for each message
-        # 3. Make POST request using requests.post() with:
-        #   - URL: self._endpoint
-        #   - headers: headers from step 1
-        #   - json: request_data from step 2
-        # 4. Get content from response, print it and return message with assistant role and content
-        # 5. If status code != 200 then raise Exception with format: f"HTTP {response.status_code}: {response.text}"
-        raise NotImplementedError
+        request_data = {
+            messages: [msg.to_dict() for msg in messages]
+        }
+
+        response = requests.post(self._endpoint, headers=self.headers, json=request_data)
+        print(response.json())
+
+        if response.status_code != 200:
+            raise Exception(f"HTTP {response.status_code}: {response.text}")
+
+        content = response.json().get['choices'][0]['message']['content']
+        return Message(role=Role.AI, content=content)
 
     async def stream_completion(self, messages: list[Message]) -> Message:
-        #TODO:
-        # Take a look at README.md of how the request and streamed response chunks are looks like!
-        # 1. Create headers dict with api-key and Content-Type
-        # 2. Create request_data dictionary with:
-        #    - "stream": True  (enable streaming)
-        #    - "messages": convert messages list to dict format using msg.to_dict() for each message
-        # 3. Create empty list called 'contents' to store content snippets
-        # 4. Create aiohttp.ClientSession() using 'async with' context manager
-        # 5. Inside session, make POST request using session.post() with:
-        #    - URL: self._endpoint
-        #    - json: request_data from step 2
-        #    - headers: headers from step 1
-        #    - Use 'async with' context manager for response
-        # 6. Get content from chunks (don't forget that chunk start with `data: `, final chunk is `data: [DONE]`), print
-        #    chunks, collect them and return as assistant message
-        raise NotImplementedError
+        request_data = {
+            "stream": True,
+            "messages": [msg.to_dict() for msg in messages]
+        }
+        contents = []
+        async with aiohttp.ClientSession() as session:
+            async with session.post(self._endpoint, headers=self.headers, json=request_data) as response:
+                if response.status != 200:
+                    raise Exception(f"HTTP {response.status}")
 
+                async for line in response.content:
+                    if line.startswith(b"data: ") and b"[DONE]" not in line:
+                        content_chunk = json.loads(line[6:])['choices'][0]['delta'].get('content', '')
+                        if content_chunk:
+                            contents.append(content_chunk)
+                            print(content_chunk, end='', flush=True)
+        print()
+        return Message(role=Role.AI, content=''.join(contents))
